140d9d39e8aa6b5a17f54a7c2316042eaf266e22
Adam <adamhool414@gmail.com>
2016-01-04 13:42:48 -0600

    part2 if loop

--- PCJs.js
+++ PCJs.js
@@ -1,6 +1,24 @@
-"use stict";
-var hours=37;
-var wage=22.5;
+"use strict";
+var hours = 0;
+var wage = 0;
+var name;
 var totalIncome;
+var overtimeHours=0;
+var overtimeWage=1.5*wage;
+var overtimeMoney=0;
+
+name = prompt("Please enter your name?");
+wage = prompt("what is your hourly wage?");
+hours = prompt("how many hours did u work?");
+if (hours>40)
+{
+	overtimeHours=hours-40;
+	overtimeMoney = overtimeHours*overtimeWage;
+	totalIncome = overtimeMoney + totalIncome;
+}
 totalIncome=hours*wage;
-console.log(totalIncome);
+console.log(wage);
+console.log(hours);
+
+
+console.log(name +" made "+ totalIncome + " on last pay check");
